82acb1f92920508178eef0dd82c3a58f5f3a9727
loic <loic@debian.home>
2024-01-21 07:07:58 +0100

    chore: added dark mode toggle for storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,6 @@
 import type { Preview } from "@storybook/react";
+import { withThemeByClassName } from "@storybook/addon-themes";
+import "../src/app/globals.css";
 
 const preview: Preview = {
   parameters: {
@@ -6,10 +8,33 @@ const preview: Preview = {
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/i,
-      },
+        date: /Date$/i
+      }
     },
-  },
+    backgrounds: {
+      default: "White",
+      values: [
+        {
+          name: "White",
+          value: "#ffffff"
+        },
+        {
+          name: "Black",
+          value: "#000000"
+        }
+      ]
+    }
+  }
 };
+
+export const decorators = [
+  withThemeByClassName({
+    themes: {
+      light: "light",
+      dark: "dark"
+    },
+    defaultTheme: "light"
+  })
+];
 
 export default preview;
